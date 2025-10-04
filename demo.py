f54f9dd75100ed05b708526842bce51ce65d6a37
thomasdang-ca <thomasdang2001@gmail.com>
2025-10-04 15:27:58 -0700

    demo.py

--- demo.py
+++ demo.py
@@ -7,10 +7,10 @@ import pandas as pd
 import seaborn as sns
     # Load the image
 
-
-img = Image.open("jayce.jpg").convert('RGB')
-img_np = np.array(img)
-
+img_1 = Image.open("jayce.jpg").convert('RGB')
+img_2 = Image.open("pussy.png").convert('RGB')
+img_np_1 = np.array(img_1)
+img_np_2 = np.array(img_2)
 
 def extract_color_palette(img_np):
   
@@ -22,16 +22,16 @@ def extract_color_palette(img_np):
 
     colors = kmeans.cluster_centers_.astype(int)
     labels = kmeans.labels_
-    print(colors)
     arr = [colors, labels]
     return arr
 
-new_arr = extract_color_palette(img_np)
+new_arr_1 = extract_color_palette(img_np_1)
+new_arr_2 = extract_color_palette(img_np_2)
 
 def recolor_clusters(img_np, labels, palette_old, palette_new): 
     H, W, C = img_np.shape
     pixels = img_np.reshape(-1, 3).astype(float)
-    
+    print(len(palette_new))
     for k in range(len(palette_new)):
         # Compute shift vector for this cluster
         shift = palette_new[k] - palette_old[k]
@@ -43,8 +43,10 @@ def recolor_clusters(img_np, labels, palette_old, palette_new):
     
     # Reshape back to original image shape
     recolored_img = pixels.reshape(H, W, C).astype(np.uint8)
-    print(recolored_img.tolist())
     return recolored_img
 
-colour = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
-recolor_clusters(img_np, new_arr[1], new_arr[0], colour)
+colour = [[255,255,255],[255,255,255],[255,255,255],[255,255,255],[255,255,255],[255,255,255]]
+print(new_arr_2[0])
+image_gray = Image.fromarray(recolor_clusters(img_np_1, new_arr_1[1], new_arr_1[0], new_arr_2[0]))
+image_gray.show()
+
