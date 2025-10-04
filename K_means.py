8a5f807de705df4e120b3bc3e45180f5ed442cf3
grantjde <144576784+grantjde@users.noreply.github.com>
2025-10-04 14:31:43 -0700

    Create K_means.py

---
+++ K_means.py
@@ -0,0 +1,22 @@
+import numpy as np
+from sklearn.cluster import KMeans
+
+def extract_color_palette(img_np, n_colors=5):
+  
+    pixels = img_np.reshape(-1, 3)
+
+    numnode = len(img_np)/200
+    if numnode < 3:
+      numnode = 3:
+    if numnode > 6:
+      numnode = 6:
+      
+    kmeans = KMeans(n_clusters=numnode, random_state=42)
+    kmeans.fit(pixels)
+
+    
+    colors = kmeans.cluster_centers_.astype(int)
+
+    labels = kmeans.labels_
+
+    return colors, labels
